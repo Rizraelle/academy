4e695484e8750d108971ebb34991a39c118a79ac
Rizraelle <rizraelleshinobu@gmail.com>
2018-11-13 17:20:08 +0300

    fix errors from css-linter

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
 //Hamburger icon
 
-document.getElementById('gamburger-nav').onclick = function(event){
+document.getElementById('hamburger-nav').onclick = function(event){
     event.target.classList.toggle("active");
     document.getElementsByClassName('header-wrapper')[0].classList.toggle("active");
 };
